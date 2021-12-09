f8db9036ba5aabee24d98bb5bcb39d7c1f3e8f65
Данила <62004037+Mitaforost@users.noreply.github.com>
2021-12-09 21:00:41 +0300

    Labs
    Labs

---
+++ Gym/Gym/Form1.Designer.cs
@@ -0,0 +1,225 @@
+﻿
+namespace Gym
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textSearch = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textSeach2 = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.saveButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(15, 35);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.Size = new System.Drawing.Size(757, 184);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(302, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(183, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "База данных Тренеров и клиентов";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 232);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 30);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Вывести тренеров";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(15, 268);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 30);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Вывести клиентов";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textSearch
+            // 
+            this.textSearch.Location = new System.Drawing.Point(280, 238);
+            this.textSearch.Name = "textSearch";
+            this.textSearch.Size = new System.Drawing.Size(275, 20);
+            this.textSearch.TabIndex = 4;
+            this.textSearch.Click += new System.EventHandler(this.textSearch_Click);
+            this.textSearch.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textSearch_KeyDown);
+            this.textSearch.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textSearch_KeyPress);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(185, 241);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(89, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Поиск тренеров";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(185, 277);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Поиск студентов";
+            // 
+            // textSeach2
+            // 
+            this.textSeach2.Location = new System.Drawing.Point(280, 274);
+            this.textSeach2.Name = "textSeach2";
+            this.textSeach2.Size = new System.Drawing.Size(275, 20);
+            this.textSeach2.TabIndex = 6;
+            this.textSeach2.Click += new System.EventHandler(this.textSeach2_Click);
+            this.textSeach2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textSeach2_KeyPress);
+            // 
+            // addButton
+            // 
+            this.addButton.Location = new System.Drawing.Point(561, 232);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(94, 30);
+            this.addButton.TabIndex = 8;
+            this.addButton.Text = "Добавить";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // saveButton
+            // 
+            this.saveButton.Location = new System.Drawing.Point(678, 232);
+            this.saveButton.Name = "saveButton";
+            this.saveButton.Size = new System.Drawing.Size(94, 30);
+            this.saveButton.TabIndex = 9;
+            this.saveButton.Text = "Сохранить";
+            this.saveButton.UseVisualStyleBackColor = true;
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Location = new System.Drawing.Point(561, 268);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(94, 30);
+            this.deleteButton.TabIndex = 10;
+            this.deleteButton.Text = "Удалить";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(15, 317);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 347);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(15, 379);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "label6";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.saveButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textSeach2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textSearch);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textSearch;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textSeach2;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button saveButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Gym/Gym/Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Npgsql;
+
+namespace Gym
+{
+    public partial class Form1 : Form
+    {
+        DataSet ds;
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            NpgsqlConnection OpiConnection = new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=postgres;Password=z7AEwer79;Database=OPIgym;");
+            NpgsqlCommand TableConnection = new NpgsqlCommand();
+            OpiConnection.Open();
+            TableConnection.Connection = OpiConnection;
+            TableConnection.CommandType = CommandType.Text;
+            TableConnection.CommandText = "select * from coach";
+            NpgsqlDataReader db = TableConnection.ExecuteReader();
+            if (db.HasRows)
+            {
+                DataTable dt = new DataTable();
+                dt.Load(db);
+                dataGridView1.DataSource = dt;
+            }
+            TableConnection.Dispose();
+            OpiConnection.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            NpgsqlConnection OpiConnection = new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=postgres;Password=z7AEwer79;Database=OPIgym;");
+            NpgsqlCommand TableConnection = new NpgsqlCommand();
+            OpiConnection.Open();
+            TableConnection.Connection = OpiConnection;
+            TableConnection.CommandType = CommandType.Text;
+            TableConnection.CommandText = "select * from students";
+            NpgsqlDataReader db = TableConnection.ExecuteReader();
+            if (db.HasRows)
+            {
+                DataTable dt = new DataTable();
+                dt.Load(db);
+                dataGridView1.DataSource = dt;
+            }
+            TableConnection.Dispose();
+            OpiConnection.Close();
+        }
+
+        private void textSearch_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            NpgsqlConnection OpiConnection = new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=postgres;Password=z7AEwer79;Database=OPIgym;");
+            NpgsqlCommand TableConnection = new NpgsqlCommand();
+            OpiConnection.Open();
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter("select * from coach", OpiConnection);
+            DataTable dt = new DataTable("coach");
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dt.DefaultView.RowFilter = String.Format("name_coach like '%" + textSearch.Text + "%'");
+            TableConnection.Dispose();
+            OpiConnection.Close();
+        }
+
+        private void textSeach2_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            NpgsqlConnection OpiConnection = new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=postgres;Password=z7AEwer79;Database=OPIgym;");
+            NpgsqlCommand TableConnection = new NpgsqlCommand();
+            OpiConnection.Open();
+            NpgsqlDataAdapter da = new NpgsqlDataAdapter("select * from students", OpiConnection);
+            DataTable dt = new DataTable("students");
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            dt.DefaultView.RowFilter = String.Format("name_students like '%" + textSeach2.Text + "%'");
+
+            TableConnection.Dispose();
+            OpiConnection.Close();
+        }
+
+        private void textSearch_Click(object sender, EventArgs e)
+        {
+            textSeach2.Text = "";
+        }
+
+        private void textSeach2_Click(object sender, EventArgs e)
+        {
+            textSearch.Text = "";
+        }
+
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            //NpgsqlConnection OpiConnection = new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=postgres;Password=z7AEwer79;Database=OPIgym;");
+            //NpgsqlCommand TableConnection = new NpgsqlCommand();
+            //OpiConnection.Open();
+            
+            //DataRow row = ds.Tables[0].NewRow(); // добавляем новую строку в DataTable
+            //ds.Tables[0].Rows.Add(row);
+            //TableConnection.Dispose();
+            //OpiConnection.Close();
+        }
+
+        private void textSearch_KeyDown(object sender, KeyEventArgs e)
+        {
+            //NpgsqlConnection OpiConnection = new NpgsqlConnection(@"Server=localhost;Port=5432;User Id=postgres;Password=z7AEwer79;Database=OPIgym;");
+            //NpgsqlCommand TableConnection = new NpgsqlCommand();
+            //OpiConnection.Open();
+            //TableConnection.CommandText = "SELECT name_coach, phone_coach from coach inner join students on coach.coach_id = students.fk_students_coach";
+            //NpgsqlDataAdapter da = new NpgsqlDataAdapter("select * from students", OpiConnection);
+            //DataTable dt = new DataTable("coach");
+            //da.Fill(dt);
+            //label4.Text = dt.Rows[0].Field<string>("name_coach");
+            //label5.Text = dt.Rows[0].Field<string>("phone_coach");
+
+        }
+    }
+}
