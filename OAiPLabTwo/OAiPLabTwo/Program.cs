3a21115fcba02c2f25568331b9d351f3ae576260
Данила <62004037+Mitaforost@users.noreply.github.com>
2022-01-07 16:53:20 +0300

    se
    se

---
+++ OAiPLabTwo/OAiPLabTwo/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAiPLabTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 10 Вариант. Даны числа A, B и C. A является делителем, B - делимым, а C, в свою очередь,
+            // является конечной степенью числа A.Вычислить сумму частных от А и B для 
+            // каждого возведения в степень числа A и вывести в консоль
+            Console.WriteLine("Введите число A делитель: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите число B делимое: ");
+            int B = Convert.ToInt32(Console.ReadLine());
+            if ((A > B && A % B == 0) || (B > A && B % A == 0) || B == A)
+            {
+                Console.WriteLine("Да, одно из чисел является делителем другого");
+            }
+            else
+            {
+                Console.WriteLine("Нет, ни одно из чисел не является делителем другого");
+            }
+            Console.ReadLine();
+        }
+    }
+}
