35b13ce31b6075076d4c75cc39c7dc8355e154fd
Данила <62004037+Mitaforost@users.noreply.github.com>
2022-01-07 16:16:32 +0300

    ad
    ad

---
+++ OAiPKR/OAiPKR/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAiPKR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for (int i = -n; i < n; i++)
+            {
+                if (i / 100 % 2 == 0)
+                {
+                    if (i / 100 != 0)
+                    {
+                        Console.Write(i + ", ");
+                    }
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
