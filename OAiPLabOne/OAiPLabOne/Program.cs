2d76213cd3f0dc1fc437bcb74e4f3289cfe3644f
Данила <62004037+Mitaforost@users.noreply.github.com>
2022-01-07 16:30:37 +0300

    qw
    qw

---
+++ OAiPLabOne/OAiPLabOne/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OAiPLabOne
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите числа A, B, C, D: ");
+            int A = Convert.ToInt32(Console.ReadLine());
+            int B = Convert.ToInt32(Console.ReadLine());
+            int C = Convert.ToInt32(Console.ReadLine());
+            int D = Convert.ToInt32(Console.ReadLine());
+            if ((A>B) && (B>C) && (C>D))
+            {
+                Console.WriteLine("Убывающую");
+            }
+            else
+            {
+                Console.WriteLine("Не убывающую");
+            }
+            Console.ReadLine();
+        }
+    }
+}
