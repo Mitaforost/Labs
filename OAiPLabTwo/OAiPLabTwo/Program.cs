c0b2e189eb458caba445835bf5ce558b93071ec0
Данила <62004037+Mitaforost@users.noreply.github.com>
2022-01-15 11:20:08 +0300

    qw
    qw

--- OAiPLabTwo/OAiPLabTwo/Program.cs
+++ OAiPLabTwo/OAiPLabTwo/Program.cs
@@ -13,18 +13,23 @@ namespace OAiPLabTwo
             // 10 Вариант. Даны числа A, B и C. A является делителем, B - делимым, а C, в свою очередь,
             // является конечной степенью числа A.Вычислить сумму частных от А и B для 
             // каждого возведения в степень числа A и вывести в консоль
-            Console.WriteLine("Введите число A делитель: ");
-            int A = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Введите число B делимое: ");
-            int B = Convert.ToInt32(Console.ReadLine());
-            if ((A > B && A % B == 0) || (B > A && B % A == 0) || B == A)
+            int A, B;
+            do
             {
-                Console.WriteLine("Да, одно из чисел является делителем другого");
-            }
-            else
-            {
-                Console.WriteLine("Нет, ни одно из чисел не является делителем другого");
-            }
+                Console.WriteLine("Введите число A делитель: ");
+                A = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Введите число B делимое: ");
+                B = Convert.ToInt32(Console.ReadLine());
+
+                if ((A > B && A % B == 0) || (B > A && B % A == 0) || B == A)
+                {
+                    Console.WriteLine("Да, одно из чисел является делителем другого");
+                }
+                else
+                {
+                    Console.WriteLine("Нет, ни одно из чисел не является делителем другого");
+                }
+            } while (A >= 0);
             Console.ReadLine();
         }
     }
